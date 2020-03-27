807e95a2ab7fe89d8e1056e8cd1cf99ee2bbc0da
kkokosa <kkokosa@f28-studio.com>
2020-03-27 11:06:48 +0100

    Chapter 3 - object address IL class rename

--- Chapter3.CIL/Class1.cs
+++ Chapter3.CIL/Class1.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace Chapter3.CIL
 {
-    public static class Class1
+    public static class ObjectExtensionsCIL
     {
         [MethodImpl(MethodImplOptions.ForwardRef)]
         public static extern Int64 GetAddress(this object obj);
