cad23d6924b0c7a4d0d32bce3cd44f69a1c6e10a
kkokosa <kkokosa@f28-studio.com>
2020-05-11 13:12:30 +0200

    Chapter 4 - byref-like instance fields not allowed

---
+++ Chapter4.Examples/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Chapter4.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(Guid.NewGuid().ToString()), AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("DynamicModule");
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("EvilType",
+                    TypeAttributes.Public |
+                    TypeAttributes.Class |
+                    TypeAttributes.AutoClass |
+                    TypeAttributes.AnsiClass |
+                    TypeAttributes.BeforeFieldInit |
+                    TypeAttributes.AutoLayout,
+                    null);
+
+            var coreLibAssembly = typeof(string).Assembly;
+            var genericByRefLikeType = coreLibAssembly.GetType("System.ByReference`1");
+            var constructedByRefLikeType = genericByRefLikeType.MakeGenericType(typeof(string));
+
+            typeBuilder.DefineField("_byRefLikeInstanceField", 
+                                    constructedByRefLikeType, 
+                                    FieldAttributes.Public);
+
+            try
+            {
+                TypeInfo objectTypeInfo = typeBuilder.CreateTypeInfo();
+
+                var evilType = objectTypeInfo.AsType();
+                var evilTypeInstance = Activator.CreateInstance(evilType);
+            }
+            catch (TypeLoadException ex)
+            {
+                // We do expect it with the message:
+                //  A ByRef-like type cannot be used as the type for an instance field in a non-ByRef-like type
+                Console.WriteLine($"Expected exception {ex}");
+            }
+        }
+    }
+}
