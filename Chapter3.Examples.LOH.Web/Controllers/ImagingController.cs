8d46757498d71922f2c07bbbfc1f451a334e96a9
kkokosa <kkokosa@f28-studio.com>
2020-10-08 15:11:17 +0200

    Binary upload examples - initial commit

---
+++ Chapter3.Examples.LOH.Web/Controllers/ImagingController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Chapter3.Examples.LOH.Web.Controllers
+{
+    /** Different ways of getting an image data from the POST.
+     */
+    [ApiController]
+    [Route("[controller]")]
+    public class ImagingController : ControllerBase
+    {
+        private readonly ILogger<ImagingController> _logger;
+
+        // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
+        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
+
+        public ImagingController(ILogger<ImagingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = reader.ReadToEnd();
+                // Do something
+            }
+            return "Ok";
+        }
+
+        [HttpGet]
+        public async Task<string> GetAsync()
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = await reader.ReadToEndAsync();
+                // Do something
+            }
+            return "Ok";
+        }
+
+        [HttpGet]
+        public async Task<byte[]> GetBytesAsync()
+        {
+            using (var ms = new MemoryStream(2048))
+            {
+                await Request.Body.CopyToAsync(ms);
+                return ms.ToArray();  // returns base64 encoded string JSON result
+            }
+        }
+    }
+}
